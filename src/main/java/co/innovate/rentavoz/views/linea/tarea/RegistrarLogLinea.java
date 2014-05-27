8543f5d596a2cb3ba38b32542f77a1ca92af2075
Elmer Jose Diaz Lazo <elmerdiazlazo@gmail.com>
2014-05-27 15:59:59 -0500

    mejora en repos

--- src/main/java/co/innovate/rentavoz/views/linea/tarea/RegistrarLogLinea.java
+++ src/main/java/co/innovate/rentavoz/views/linea/tarea/RegistrarLogLinea.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
@@ -22,13 +23,17 @@ import org.apache.log4j.Logger;
 import org.primefaces.event.FileUploadEvent;
 
 import co.innovate.rentavoz.model.almacen.Linea;
+import co.innovate.rentavoz.model.facturacion.NotaDebito;
 import co.innovate.rentavoz.model.log.linea.AccionLineaEnum;
 import co.innovate.rentavoz.model.log.linea.LogLinea;
 import co.innovate.rentavoz.repositories.linea.impl.LineaDaoImpl;
 import co.innovate.rentavoz.services.estadolinea.EstadoLineaService;
+import co.innovate.rentavoz.services.facturacion.NotaDebitoService;
 import co.innovate.rentavoz.services.linea.LineaService;
 import co.innovate.rentavoz.services.log.linea.LogLineaService;
+import co.innovate.rentavoz.services.tercero.TerceroService;
 import co.innovate.rentavoz.views.BaseBean;
+import co.innovate.rentavoz.views.components.autocomplete.AutocompleteTercero;
 import co.innovate.rentavoz.views.session.Login;
 
 import com.sun.faces.context.ExternalContextImpl;
@@ -63,16 +68,46 @@ public class RegistrarLogLinea extends BaseBean implements Serializable {
 
 	private String tarea;
 
+	private double valorRepo;
+	
+	
+	private AutocompleteTercero autocompleteTercero;
+
 	@ManagedProperty(value = "#{lineaService}")
 	private LineaService lineaService;
-	
-	@ManagedProperty(value="#{estadoLineaService}")
+
+	@ManagedProperty(value = "#{estadoLineaService}")
 	private EstadoLineaService estadoLineaService;
+
+	@ManagedProperty(value="#{terceroService}")
+	private TerceroService terceroService;
+	
+	
+	@ManagedProperty(value="#{notaDebitoService}")
+	private NotaDebitoService notaDebitoService;
 	
 	private OutputStream out;
-	
+
+
 	private Logger logger = Logger.getLogger(RegistrarLogLinea.class);
 
+	
+	@PostConstruct
+	public void init(){
+		
+		autocompleteTercero=new AutocompleteTercero() {
+			
+			@Override
+			public void postSelect() {
+				
+			}
+			
+			@Override
+			public TerceroService getService() {
+				return terceroService;
+			}
+		};
+	}
 	/**
 	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
 	 * @date 16/03/2014
@@ -83,81 +118,88 @@ public class RegistrarLogLinea extends BaseBean implements Serializable {
 			mensajeError("No se ha encontrado esta linea en el sistema");
 			return;
 		}
-		
-		logLinea=new LogLinea();
+
+		logLinea = new LogLinea();
 		logLinea.setAccion(AccionLineaEnum.valueOf(tarea));
-		
+
 		logLinea.setLinea(linea);
 		logLinea.setFecha(Calendar.getInstance().getTime());
 		logLinea.setUsuarioCrea(login.getTercero());
-		
+
 		lineas.add(logLinea);
-		
 
 	}
 
-	
-	public void guardar(){
+	public void guardar() {
 		for (LogLinea logLinea : lineas) {
 			logLineaService.save(logLinea);
-			Linea linea=logLinea.getLinea();
+			Linea linea = logLinea.getLinea();
 			switch (logLinea.getAccion()) {
 			case LOCAL:
-				linea.setEstadoLineaidEstadoLinea(estadoLineaService.findById(LineaDaoImpl.ESTADO_LINEA_LOCAL));
+				linea.setEstadoLineaidEstadoLinea(estadoLineaService
+						.findById(LineaDaoImpl.ESTADO_LINEA_LOCAL));
 				break;
 
 			case SUSPENDER:
-				linea.setEstadoLineaidEstadoLinea(estadoLineaService.findById(LineaDaoImpl.ESTADO_LINEA_SUSPENDIDA));
+				linea.setEstadoLineaidEstadoLinea(estadoLineaService
+						.findById(LineaDaoImpl.ESTADO_LINEA_SUSPENDIDA));
 				break;
+				case REPO:
+					
+					NotaDebito notaDebito = new NotaDebito();
+					notaDebito.setConcepto("Reposicion de linea "+logLinea.getLinea().getLinNumero());
+					notaDebito.setSucursal(login.getSucursalContable());
+					notaDebito.setCliente(autocompleteTercero.getSeleccionado());
+					notaDebito.setCreador(login.getTercero());
+					notaDebito.setFechaEmision(Calendar.getInstance().getTime());
+					notaDebito.setValor(valorRepo);
+					notaDebitoService.save(notaDebito);
+					break;
 			default:
 				break;
 			}
 			lineaService.save(linea);
-			
+
 		}
-		lineas=new ArrayList<LogLinea>();
-		numeroLinea=StringUtils.EMPTY;
+		lineas = new ArrayList<LogLinea>();
+		numeroLinea = StringUtils.EMPTY;
 		mensaje("Realizado", "Se han guardado las transacciones correctamente");
-		
-		
+
 	}
-	
 
-	public void cargarRepos(FileUploadEvent event){
+	public void cargarRepos(FileUploadEvent event) {
 		try {
-			
-		
-		InputStream in = event.getFile().getInputstream();
-		String fileName = event.getFile().getFileName();
-		fileName=event.getFile().getFileName().replace(" ", "").trim();
 
-		ExternalContextImpl request;
-		request = (ExternalContextImpl) FacesContext.getCurrentInstance()
-				.getExternalContext();
-		StringBuilder path = new StringBuilder( request.getRealPath("/"));
-		out = new FileOutputStream(path
-				+ fileName);
-		 new File(request.getRealPath(path
-				+ fileName));
+			InputStream in = event.getFile().getInputstream();
+			String fileName = event.getFile().getFileName();
+			fileName = event.getFile().getFileName().replace(" ", "").trim();
 
-		if (in != null) {
-			int b = 0;
-			while (b != -1) {
-				b = in.read();
-				if (b != -1) {
-					out.write(b);
+			ExternalContextImpl request;
+			request = (ExternalContextImpl) FacesContext.getCurrentInstance()
+					.getExternalContext();
+			StringBuilder path = new StringBuilder(request.getRealPath("/"));
+			out = new FileOutputStream(path + fileName);
+			new File(request.getRealPath(path + fileName));
+
+			if (in != null) {
+				int b = 0;
+				while (b != -1) {
+					b = in.read();
+					if (b != -1) {
+						out.write(b);
+
+					}
 
 				}
-
 			}
-		}
-		
-		lineas=logLineaService.cargarLineaScid(path
-				+ fileName,login.getTercero());
+
+			lineas = logLineaService.cargarLineaScid(path + fileName,
+					login.getTercero());
 		} catch (Exception e) {
 			logger.error(e);
 		}
 	}
+
 	@ManagedProperty(value = "#{login}")
 	private Login login;
 
@@ -266,12 +308,71 @@ public class RegistrarLogLinea extends BaseBean implements Serializable {
 	public void setLineaService(LineaService lineaService) {
 		this.lineaService = lineaService;
 	}
+
 	/**
 	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
 	 * @date 17/03/2014
-	 * @param estadoLineaService the estadoLineaService to set
+	 * @param estadoLineaService
+	 *            the estadoLineaService to set
 	 */
 	public void setEstadoLineaService(EstadoLineaService estadoLineaService) {
 		this.estadoLineaService = estadoLineaService;
 	}
+
+	/**
+	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 27/05/2014
+	 * @param valorRepo
+	 *            the valorRepo to set
+	 */
+	public void setValorRepo(double valorRepo) {
+		this.valorRepo = valorRepo;
+	}
+
+	/**
+	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 27/05/2014
+	 * @return the valorRepo
+	 */
+	public double getValorRepo() {
+		return valorRepo;
+	}
+	
+	/**
+	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 27/05/2014
+	 * @param autocompleteTercero the autocompleteTercero to set
+	 */
+	public void setAutocompleteTercero(AutocompleteTercero autocompleteTercero) {
+		this.autocompleteTercero = autocompleteTercero;
+	}
+	
+	/**
+	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 27/05/2014
+	 * @return the autocompleteTercero
+	 */
+	public AutocompleteTercero getAutocompleteTercero() {
+		return autocompleteTercero;
+	}
+	
+	/**
+	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 27/05/2014
+	 * @param notaDebitoService the notaDebitoService to set
+	 */
+	public void setNotaDebitoService(NotaDebitoService notaDebitoService) {
+		this.notaDebitoService = notaDebitoService;
+	}
+	
+	/**
+	 * @author <a href="elmerdiazlazo@gmail.com">Elmer Jose Diaz Lazo</a>
+	 * @date 27/05/2014
+	 * @param terceroService the terceroService to set
+	 */
+	public void setTerceroService(TerceroService terceroService) {
+		this.terceroService = terceroService;
+	}
+	
+	
 }
